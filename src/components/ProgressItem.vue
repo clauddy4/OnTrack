2c6b37dd58e42337ff66df2232d8c5ac98c55286
Olga Makarova <53290930+clauddy4@users.noreply.github.com>
2023-09-17 09:57:43 +0300

    Isolating global modules

--- src/components/ProgressItem.vue
+++ src/components/ProgressItem.vue
@@ -2,25 +2,35 @@
 import { computed } from 'vue'
 import { getActivityProgress } from '@/activities'
 import { getProgressColorClass } from '@/functions'
+import { getTotalActivitySeconds } from '@/timeline-items'
+import { formatSeconds } from '@/functions'
+import { isActivityValid } from '@/validators'
 
-const props = defineProps(['index', 'activity'])
+const props = defineProps({
+  activity: {
+    type: Object,
+    required: true,
+    validator: isActivityValid
+  }
+})
 
-const timeProgress = ['03:00 / 30:00', '15:00 / 30:00', '21:00 / 30:00', '30:00 / 30:00'][
-  props.index
-]
-
-const progress = computed(() => getActivityProgress(props.activity))
+const percentage = computed(() =>
+  getActivityProgress(props.activity, getTotalActivitySeconds(props.activity))
+)
 </script>
 
 <template>
   <li class="flex flex-col gap-1 p-4">
     <div class="truncate text-xl">{{ activity.name }}</div>
     <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
-      <div :class="getProgressColorClass(progress)" :style="`width: ${progress}%`" />
+      <div :class="getProgressColorClass(percentage)" :style="`width: ${percentage}%`" />
     </div>
     <div class="flex justify-between font-mono text-sm">
-      <span>{{ progress }}%</span>
-      <span>{{ timeProgress }}</span>
+      <span>{{ percentage }}%</span>
+      <span>
+        {{ formatSeconds(getTotalActivitySeconds(activity)) }} /
+        {{ formatSeconds(activity.secondsToComplete) }}
+      </span>
     </div>
   </li>
 </template>
